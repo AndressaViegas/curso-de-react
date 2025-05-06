16f68f129cb7893d6786a5262da693d51648a8a4
Andressa viegas <andressaviegas425@gmail.com>
2025-05-06 18:53:28 -0300

    ajusta o tamanho do texto e o padding do componente Title para melhorar a legibilidade

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,5 +1,5 @@
 function Title(props) {
-    return <h1 className="text-3xl text-slate-100 font-bold text-center m-2">
+    return <h1 className="text-sm p-4 text-slate-100 font-bold text-center m-2">
         {props.children}
     </h1>
 
